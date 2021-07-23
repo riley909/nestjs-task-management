cc283ebc0286c3bf2c25185fed098d7728b38b8d
riley909 <increaseblue666@gmail.com>
2021-07-23 23:21:02 +0900

    add getTaskById

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -11,6 +11,10 @@ export class TasksService {
     return this.tasks;
   }
 
+  getTaskById(id: string): Task {
+    return this.tasks.find((task) => task.id === id);
+  }
+
   createTask(createTaskDto: createTaskDto): Task {
     const { title, description } = createTaskDto;
     const task: Task = {
