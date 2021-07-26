ea4b9ce44ad46c388593510708a9441e1b630d81
riley909 <increaseblue666@gmail.com>
2021-07-26 23:57:00 +0900

    remove unused import

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './tasks.model';
 import { v4 as uuid } from 'uuid';
 import { createTaskDto } from './dto/create-task.dto';
-import { updateTaskDto } from './dto/update-task.dto';
 
 @Injectable()
 export class TasksService {
