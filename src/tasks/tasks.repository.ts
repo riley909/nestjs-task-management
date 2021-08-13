b2bdfb1f4732a49a925b74176304c663948be223
riley909 <increaseblue666@gmail.com>
2021-08-13 22:41:02 +0900

    create TasksRepository

--- src/tasks/tasks.repository.ts
+++ src/tasks/tasks.repository.ts
@@ -1,5 +1,25 @@
 import { EntityRepository, Repository } from 'typeorm';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { Task } from './task.entity';
 
 @EntityRepository(Task)
-export class TasksRespository extends Repository<Task> {}
+export class TasksRepository extends Repository<Task> {
+  async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+    const { status, search } = filterDto;
+    const query = this.createQueryBuilder('task');
+
+    if (status) {
+      query.andWhere('task.status = :status', { status });
+    }
+
+    if (search) {
+      query.andWhere(
+        'LOWER(task.title) LIKE LOWER(:search) OR LOWER(task.description) LIKE LOWER(:search)',
+        { search: `%${search}%` },
+      );
+    }
+
+    const tasks = await query.getMany();
+    return tasks;
+  }
+}
