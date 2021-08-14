32c2d0a20496ce55fd96783d58444fb9a9217d97
riley909 <increaseblue666@gmail.com>
2021-08-14 23:38:12 +0900

    add createTask

--- src/tasks/tasks.repository.ts
+++ src/tasks/tasks.repository.ts
@@ -1,5 +1,7 @@
 import { EntityRepository, Repository } from 'typeorm';
+import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { TaskStatus } from './task-status.enum';
 import { Task } from './task.entity';
 
 @EntityRepository(Task)
@@ -21,5 +23,17 @@ export class TasksRepository extends Repository<Task> {
 
     const tasks = await query.getMany();
     return tasks;
+  }
+
+  async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+    const { title, description } = createTaskDto;
+
+    const task = this.create({
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    });
+    await this.save(task);
+    return task;
   }
 }
