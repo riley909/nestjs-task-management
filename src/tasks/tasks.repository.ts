3c541ca03db03d634d9eea6befd52ddabdf0b588
riley909 <increaseblue666@gmail.com>
2021-08-23 21:48:57 +0900

    add user param on getTasksById

--- src/tasks/tasks.repository.ts
+++ src/tasks/tasks.repository.ts
@@ -19,7 +19,7 @@ export class TasksRepository extends Repository<Task> {
 
     if (search) {
       query.andWhere(
-        'task.title ILIKE :search OR task.description ILIKE :search',
+        '(task.title ILIKE :search OR task.description ILIKE :search)',
         { search: `%${search}%` },
       );
     }
@@ -28,8 +28,8 @@ export class TasksRepository extends Repository<Task> {
     return tasks;
   }
 
-  async getTaskById(id: string): Promise<Task> {
-    const found = await this.findOne(id);
+  async getTaskById(id: string, user: User): Promise<Task> {
+    const found = await this.findOne({ where: { id, user } });
     if (!found) {
       throw new NotFoundException(`Task with ID "${id}" not found`);
     }
@@ -56,8 +56,12 @@ export class TasksRepository extends Repository<Task> {
     }
   }
 
-  async updateTaskStatus(id: string, status: TaskStatus): Promise<Task> {
-    const task = await this.getTaskById(id);
+  async updateTaskStatus(
+    id: string,
+    status: TaskStatus,
+    user: User,
+  ): Promise<Task> {
+    const task = await this.getTaskById(id, user);
     task.status = status;
     await this.save(task);
     return task;
