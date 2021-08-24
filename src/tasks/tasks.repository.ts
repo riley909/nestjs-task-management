95dedde6248221c1e7db81446c36758b673fa2e5
riley909 <increaseblue666@gmail.com>
2021-08-24 10:40:21 +0900

    add log for practice

--- src/tasks/tasks.repository.ts
+++ src/tasks/tasks.repository.ts
@@ -1,13 +1,19 @@
-import { NotFoundException } from '@nestjs/common';
+import {
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { User } from 'src/auth/user.entity';
 import { EntityRepository, Repository } from 'typeorm';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { TaskStatus } from './task-status.enum';
 import { Task } from './task.entity';
+import { Logger } from '@nestjs/common';
 
 @EntityRepository(Task)
 export class TasksRepository extends Repository<Task> {
+  private logger = new Logger('TasksRepository', { timestamp: true });
+
   async getTasks(filterDto: GetTasksFilterDto, user: User): Promise<Task[]> {
     const { status, search } = filterDto;
     const query = this.createQueryBuilder('task');
@@ -24,8 +30,21 @@ export class TasksRepository extends Repository<Task> {
       );
     }
 
-    const tasks = await query.getMany();
-    return tasks;
+    try {
+      const tasks = await query.getMany();
+      return tasks;
+    } catch (error) {
+      // 에러로그 작성
+      // 1차로 내가 에러에 대한 정보를 제공
+      this.logger.error(
+        `Failed to get tasks for user "${
+          user.username
+        }. Filters: ${JSON.stringify(filterDto)}"`,
+        // 2차로 stack trace를 로그로 확인
+        error.stack,
+      );
+      throw new InternalServerErrorException();
+    }
   }
 
   async getTaskById(id: string, user: User): Promise<Task> {
